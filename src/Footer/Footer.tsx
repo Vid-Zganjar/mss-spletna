c857e070157f3a411ccefb7aa3aecb45be6e7b67
Vid-Zganjar <vid.zganjar7@gmail.com>
2024-05-16 09:09:50 +0200

    Home page done

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,40 +1,50 @@
-import React from "react";
-import { SocialIcon } from 'react-social-icons';
-import "./Footer.css";
+// Footer.js
+import React from 'react';
+import { FaFacebookF, FaInstagram, FaSnapchatGhost, FaTiktok } from 'react-icons/fa';
 import { NavLink } from "react-router-dom";
+import "./Footer.css"
 
-const scrollToTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-};
-
-const Footer = () => (
-  
-  <footer>
-  <div className="footerContainer">
-      <div className="socialIcons">
-          <i><SocialIcon  network="tiktok" url="" /></i>
-          <i><SocialIcon network="facebook" url="" /></i>
-          <i><SocialIcon network="instagram" url="" /></i>
-          <i><SocialIcon network="snapchat" url="" /></i>
+const Footer = () => {
+    return (
+      <footer className="bg-gray-900 text-white py-6">
+      <div className="container mx-auto flex flex-col items-center">
+          <div className="text-4xl mb-4 p-10">Poklikaj:</div>
+          <div className="flex space-x-12 mb-4 p-8" >
+              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="fa text-white text-5xl ">
+                  <FaFacebookF />
+              </a>
+              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="fa text-white text-5xl hover:color-green">
+                  <FaInstagram />
+              </a>
+              <a href="https://snapchat.com" target="_blank" rel="noopener noreferrer" className="fa text-white text-5xl hover:color-green">
+                  <FaSnapchatGhost />
+              </a>
+              <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer" className="fa text-white text-5xl hover:color-green">
+                  <FaTiktok />
+              </a>
+          </div>
+          <div className="flex space-x-10 mb-6 p-8">
+          <NavLink to="/novice">
+              <a href="#" className="border-b-2 border-transparent hover:border-green-500 sm:mx-10">Novice</a>
+          </NavLink>
+          <NavLink to="/trgovina">
+              <a href="#" className="border-b-2 border-transparent hover:border-green-500 sm:mx-10">Trgovina</a>
+          </NavLink>
+          <NavLink to="/about">
+              <a href="#" className="border-b-2 border-transparent hover:border-green-500 sm:mx-10">O nas</a>
+          </NavLink>
+          <NavLink to="/">
+              <a href="#" className="border-b-2 border-transparent hover:border-green-500 sm:mx-10">Domov</a>
+          </NavLink>
+          </div>
+          <div className="bg-gray-800 w-full py-2 mt-4">
+              <div className="container mx-auto text-center text-sm">
+                  &copy; Copyright {new Date().getFullYear()}  <a className='text-gray-100 font-mono text-md pl-2' href="https://zganjar.wordpress.com/">Žganjar Tech</a>
+              </div>
+          </div>
       </div>
-      
-      <div className="footerNav">
-      </div>
-  </div>
-
-
- 
-  
-  <div className="footerBottom">
-      © 2024 Copyright: 
-      <a className="text-white designer" href="https://zganjar.wordpress.com/">
-        Balin Majster d.o.o.
-      </a>
-    </div>
   </footer>
-);
+    );
+}
 
 export default Footer;
